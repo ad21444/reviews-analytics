efe520504d10c718f75d1c30cec2604eb2b1f061
Edward Hsu <ad21444@yahoo.com.tw>
2019-12-18 14:30:59 +0800

    final

--- read.py
+++ read.py
@@ -1,7 +1,15 @@
 data = []
+count = 0
+length = 0
 with open ('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
-print(len(data))
+		count += 1
+		if count % 10000 == 0:
+			print(count)
+print('總共有',len(data),'筆資料')
+for i in data:
+	length +=len(i)
 
+print(length / len(data))
 print(data[12058])
